74f1e03f91da933287d1f5c87fb5d2b40d20cd25
Mick Wright <mick.wright@protonmail.com>
2017-01-31 16:01:50 +0000

    test this

---
+++ Code/2017/forfiletype.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+cwd = os.getcwd()
+n = 1
+
+for file in os.listdir(cwd):
+	if file.endswith(".avi"):
+		video_caputre = cv2.VideoCapture(file)
+		
+		while True:
+			ret,frame = video_capture.read()
+			filename = 'frame_%d.tif' %n
+			cv2.imwrite(filename,frame)
+			n = n+1
+			print(filename)

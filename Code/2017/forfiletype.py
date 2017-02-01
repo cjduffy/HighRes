9ffdef67624df10ff4df5abc141a7e4bf7460f77
Chris <c.jduffy@me.com>
2017-02-01 08:04:34 +0000

    Updated to convert frames to fits

--- Code/2017/forfiletype.py
+++ Code/2017/forfiletype.py
@@ -1,7 +1,11 @@
 import os
 import cv2
+from astropy.io import fits
+import numpy as np
+from PIL import Image
+
 cwd = os.getcwd()
-n = 1
+n=1
 
 for file in os.listdir(cwd):
 	if file.endswith(".avi"):
@@ -9,7 +13,19 @@ for file in os.listdir(cwd):
 		
 		while True:
 			ret, frame = video_capture.read()
-			filename = 'frame_%d.tif' %n
-			cv2.imwrite(filename,frame)
+			filename_tif = 'frame_%d.tif' %n
+			cv2.imwrite(filename_tif,frame)
+			
+			hdu = fits.PrimaryHDU()
+		
+			im = Image.open('frame_%d.tif' %n)
+			hdu.data = np.array(im)
+			filename_fits = 'frame_%d.fits' %n				
+			hdu.writeto(filename_fits, overwrite=True)
+		
+			os.remove(filename_tif)
 			n = n+1
-			print(filename)
+			
+		
+			
+	

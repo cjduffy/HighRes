73d41118c6eb3835c445c9ed7fe91d3aa0cda76b
Chris <c.jduffy@me.com>
2017-02-01 20:21:19 +0000

    Dark Current and Flat field removal program

---
+++ Code/2017/darkflat.py
@@ -0,0 +1,49 @@
+#  darkflat.py
+#  
+#  Copyright 2017 Chris Duffy
+#  
+#  Currently this program of taking a file
+#  programed into it and running a dark
+#  current and flat field correction process
+# using preexisting dark and flat fields..
+#  
+#  Program will need updated to operate
+#  as a fucntion and may need some file
+#  pointers changed as approriate.
+#  
+#  
+
+
+import os
+from astropy.io import fits
+import numpy as np
+
+
+Image = fits.open(FILE_NAME)
+imagedata = Image[0].data
+
+
+##Checking the existance of Flat Field and Dark Current Images and reads them in
+if os.path.isfile('masterFF.fits'):
+	flat_field = fits.open('masterFF.fits') 
+	flatdata = flat_field[0].data
+elif os.path.isfile('FF.fits'):
+	flat_field = fits.open('FF.fits')
+	flatdata = flat_field[0].data
+else:
+	print('Error:No Flat Field')
+	#Add Error Condiditon Here
+	
+if os.path.isfile('masterDC.fits'):
+	Dark_Current = fits.open('masterDC.fits')
+	darkdata = Dark_Current[0].data
+elif os.path.isfile('DC.fits'):
+	Dark_Current = fits.open('DC.fits')
+	darkdata = Dark_Current[0].data
+else:
+		print('Error:No Dark Current')
+		#Add Error Condition Here
+		
+##Performs Dark/Flat corrections
+Dark_Removed = np.subtract(imagedata, darkdata)
+Corrected_Image = np.divide(Dark_Removed, flatdata)

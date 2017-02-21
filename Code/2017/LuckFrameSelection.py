1d0492ca556d67c53f1b882c99f92918db613c9f
Chris <c.jduffy@me.com>
2017-02-21 13:53:19 +0000

    IT WORKS

--- Code/2017/LuckFrameSelection.py
+++ Code/2017/LuckFrameSelection.py
@@ -3,27 +3,27 @@ import numdifftools as nd
 
 n=1
 m=1
+fisher_sum = 0
 
 image = np.array([[1,2],[1,2]])
 sum_pixels = np.sum(image)
 
 x,y = image.shape
-normalised_image = zeros(x,y)
+normalised_image = np.zeros((x, y))
 
 for n in range (1,x):
 	for m in range (1, y):
 		for n in range (1, x):
 			for m in range (1, y):
 				pixel = image[n,m]
-				normalised_image = np.divide(pixel, sum_pixels)
+				normalised_image[n,m] = np.divide(pixel, sum_pixels)
 			
 		sqrt_img = np.sqrt(normalised_image)
-		print(sqrt_img)
-		grad_img = np.Gradient(sqrt_img)
-		print(grad_img)
-		mag_img = abs(grad_img)
+		grad_img = np.gradient(sqrt_img)
+		mag_img = np.abs(grad_img)
 		square_img = mag_img**2
-		fisher_sum += square_img
+		fisher_sum += np.sum(square_img)
 		Fisher_Sum = 4*fisher_sum
+		print(Fisher_Sum)
 	
 		

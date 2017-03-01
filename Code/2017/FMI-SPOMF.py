914b18d70764d45b9935e5af85c9406956fd6c58
Chris <c.jduffy@me.com>
2017-03-01 22:49:04 +0000

    Quantum Tuples

--- Code/2017/FMI-SPOMF.py
+++ Code/2017/FMI-SPOMF.py
@@ -1,13 +1,27 @@
 import cv2
 import numpy as np
 from scipy.fftpack import fft2, ifft2
+from scipy.ndimage import rotate
+#Final version won't need PIL
+from PIL import Image
 
 ##Image read in
 
-##Convert to logpolar
+##Currently Hard wired for something in my working directory as I have decided how best to loop this yet.
+image1 = Image.open('outfile.jpg')
+image2 = Image.open('outfile.jpg')
 
-LP1 = cv2.logPolar(image1)
-LP2 = cv2.logPolar(image2)
+data1 = np.array(image1)
+data2 = np.array(image2)
+
+
+center1 = np.divide((data1.size), 2)
+center2 = np.divide((image2.size), 2)
+
+##Convert to logpolar
+##There is something wrong here, the documentatoin is non existant, I might actually be foreced to ask stackoverflow
+LP1 = cv2.logPolar(data1, center1, 0.1,[cv2.WARP_FILL_OUTLIERS, 1])
+LP2 = cv2.logPolar(data2, center2, 10, [cv2.WARP_FILL_OUTLIERS, 1])
 
 ##Fourier Time!
 
@@ -25,5 +39,7 @@ scaling_factor = np.exp(scale)
 
 scaled_image = np.divide(image2, scaling_factor)
 scaled_image_2 = scaled_image
- 
+
+rot1 = rotate(scaled_image, rotation)
+rot2 = rotate(scaled_image_2, rotation+180)
 

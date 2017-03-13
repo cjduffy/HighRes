028444be4dce53cf2685e7cd4fa7a2938f8169ea
Chris <c.jduffy@me.com>
2017-03-13 02:38:33 +0000

    Exceptions, Exceptions there are so many exceptions

--- Code/2017/FMI-SPOMF.py
+++ Code/2017/FMI-SPOMF.py
@@ -6,46 +6,55 @@ import math
 #Final version won't need PIL
 from PIL import Image
 
-def logpolar(image):
+def logpolar(image, i_0, j_0, p_n=None, t_n=None):
+    (i_n, j_n) = image.shape[:2]
+    
+    i_c = max(i_0, i_n - i_0)
+    j_c = max(j_0, j_n - j_0)
+    d_c = (i_c ** 2 + j_c ** 2) ** 0.5
 
-	
-	##Initialise X Counter
-	x = 1
-	
-	##Get Shape of Image
-	max_x, max_y = image.shape
-	Log_Polar_Image = np.zeros((max_x, max_y))
-	
-	##Calculate the centre position
-	center_x = max_x/2
-	center_y = max_y/2
-	
-	##Loop Through x and y:
-	for x in range(1,max_x):
-		y = 1
-		for y in range(1,max_y):
-			x_pos = x - center_x
-			y_pos = y - center_y
-			
-			r = math.sqrt(x_pos**2 + y_pos**2)
-			if x_pos != 0:
-				a_in = math.fabs(y_pos/x_pos)
-				a = np.arctan(a_in)
-			else:
-				a = 1
-			#ep = math.log10(r)
-			
-			new_x = [r*np.cos(a)]
-			new_y = [r*np.sin(a)]
-	ndii.map_coordinates(image, [new_x, new_y], output=Log_Polar_Image)		
-			
-	return(Log_Polar_Image)
+    if p_n == None:
+        p_n = int(math.ceil(d_c))
+    
+    if t_n == None:
+        t_n = j_n
+    
+    p_s = math.log(d_c) / p_n
+    t_s = 2.0 * np.pi / t_n
+    
+    transformed = np.zeros((p_n, t_n) + image.shape[2:], dtype=image.dtype)
+
+    for p in range(0, p_n):
+        p_exp = math.exp(p * p_s)
+        for t in range(0, t_n):
+            t_rad = t * t_s
+
+            i = int(i_0 + p_exp * math.sin(t_rad))
+            j = int(j_0 + p_exp * math.cos(t_rad))
+
+            if 0 <= i < i_n and 0 <= j < j_n:
+                transformed[p, t] = image[i, j]
+
+    return transformed
 	
 
 def FMI(data1, data2):
+	try:
+		max_x_1, max_y_1 = data1.shape
+		max_x_2, max_y_2 = data2.shape
+	except:
+		max_x_1, max_y_1, colour = data1.shape
+		max_x_2, max_y_2, colour = data2.shape
+		
+	##Calcylate the centres for both images
+	centre_x_1 = max_x_1/2
+	centre_y_1 = max_y_1/2
 	
-	LP1 = logpolar(data1)
-	LP2 = logpolar(data2)
+	centre_x_2 = max_x_2/2
+	centre_y_2 = max_y_2/2
+	
+	LP1 = logpolar(data1, centre_x_1, centre_y_1)
+	LP2 = logpolar(data2, centre_x_2, centre_y_2)
 	
 	##Fourier Time!
 	
@@ -56,55 +65,72 @@ def FMI(data1, data2):
 	phase_image_2 = np.imag(transform_image_2)
 		
 	if phase_image_1.shape != phase_image_2.shape:
-		x1, y1 = phase_image_1.shape
-		x2, y2 = phase_image_2.shape
 		try:
-			x = x2 - x1
-			y = y2 - y1
-			phase_image_1 = np.lib.pad(phase_image_1, ((x,0), (y,0)), 'edge')
+			x1, y1 = phase_image_1.shape
+			x2, y2 = phase_image_2.shape
+			try:
+				x = x2 - x1
+				y = y2 - y1
+				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0)), 'edge')
+			except:
+				x = x1 - x2
+				y = y1 - y2
+				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0)), 'edge')
+			
 		except:
-			x = x1 - x2
-			y = y1 - y2
-			phase_image_2 = np.lib.pad(phase_image_2, ((x,0), (y,0)), 'edge')
-		
+			x1, y1, colour = phase_image_1.shape
+			x2, y2, colour = phase_image_2.shape
+			try:
+				x = x2 - x1
+				y = y2 - y1
+				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0), (0,0) ), 'edge')
+			except:
+				x = x1 - x2
+				y = y1 - y2
+				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0), (0,0) ), 'edge')
 	
-	SPOMF = np.exp(np.array(np.subtract(phase_image_2, phase_image_1), dtype=np.float128))
-	iSPMF = ifft2(np.array((SPOMF), dtype=np.float64))
+	SPOMF = np.exp(np.subtract(phase_image_2, phase_image_1))
+	iSPMF = ifft2(SPOMF)
 	
-	return iSPMF, #log_base
+	return iSPMF
 
 ##Image read in
 
 ##Currently Hard wired for something in my working directory as I have decided how best to loop this yet.
-image1 = Image.open('outfile.jpg')
-image2 = Image.open('outfile2.jpg')
+image1 = Image.open('scifestt.jpg')
+image2 = Image.open('scifestt.jpg')
 
-data1 = np.array((image1), dtype=np.float64)
-data2 = np.array((image2), dtype=np.float64)
+data1 = np.array((image1))
+data2 = np.array((image2))
 
 iSPMF1 = FMI(data1, data2)
 
-
-rotation, scale = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
+try:
+	rotation, scale = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
+except:
+	rotation, scale, colour = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
 scaling_factor = np.exp(scale)
+print(rotation)
+print(scaling_factor)
 
+try:
+	n, m = (data2.shape/scaling_factor)
+	scaled_image = np.resize(data2, (m,n))
+	n = int(n)
+	m = int(m)
+except:
+	n, m, colour = (data2.shape/scaling_factor)
+	scaled_image = np.resize(data2, (m,n,0))
+	n = int(n)
+	m = int(m)
 
-n, m = (data2.shape/scaling_factor)
-n = int(n)
-m = int(m)
-
-
-scaled_image = np.resize(data2, (m,n))
 scaled_image_2 = scaled_image
 
-rot1 = rotate(scaled_image, rotation)
-rot2 = rotate(scaled_image_2, rotation+180)
+rot1 = rotate(scaled_image, rotation, reshape = False)
+rot2 = rotate(scaled_image_2, rotation+180, reshape = False)
 
 iSPMF2 = FMI(data1, rot1)
 iSPMF3 = FMI(data1, rot2)
-
-rotation1, scale1 = np.unravel_index(np.argmax(iSPMF2), iSPMF2.shape)
-rotation2, scale2 = np.unravel_index(np.argmax(iSPMF3), iSPMF3.shape)
 
 A = np.amax(iSPMF2)
 B = np.amax(iSPMF3)

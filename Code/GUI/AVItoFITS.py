bd14b64281bbb13902039253dca6eed623720118
Mick Wright <mick.wright@protonmail.com>
2017-03-14 13:26:49 +0000

    I AM BECOME PYTHON. DESTROYER OF ERRORS

--- Code/GUI/AVItoFITS.py
+++ Code/GUI/AVItoFITS.py
@@ -9,23 +9,32 @@ def avi_to_fits(single="none", group="none", switch=False, Imtype="frame"):
 	l = 1
 	
 	if (single != "none"):
-		for file in os.listdir(os.path.dirname(os.path.realpath(single))):
-			if file.startswith(str(Imtype)+"_"+str(l)):
-				l = l + 1 
 		n = 1
 		if single.endswith(".avi"):
+			filepath = os.path.dirname(os.path.realpath(single))
+			actual_file = os.path.basename(single)
 			video_capture = cv2.VideoCapture(single)
+			
+			filename = actual_file.replace(".avi", "")
+			folder = filepath+"/"+filename
+			
+			if not os.path.exists(folder):
+				os.mkdir(folder)
+				
+			while True:	
+				filename_initial = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
+				if os.path.isfile(filename_initial):
+					l = l + 1
+				else:
+					break
+	
 			while True:
-			#check frame is there, and read it in
 				ret, frame = video_capture.read()
-			#if at end of video, break out of loop
 				if (ret != True):
 					break
-			
-			#construct the filenames
-				filepath = os.path.dirname(os.path.realpath(single))
-				filename_tif = filepath+"/"+single+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".tif"
-				filename_fits = filepath+"/"+single+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
+					
+				filename_tif = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".tif"
+				filename_fits = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
 			
 			#write the TIF
 				cv2.imwrite(filename_tif,frame)
@@ -45,38 +54,39 @@ def avi_to_fits(single="none", group="none", switch=False, Imtype="frame"):
 			
 	elif (group != "none"):
 		for file in os.listdir(group):
-			if file.startswith(str(Imtype)+"_"+str(l)):
-				l = l + 1
-				
-		for file in os.listdir(group):
 			n = 1
 			if file.endswith(".avi"):
 				filepath = group+"/"+file
 				video_capture = cv2.VideoCapture(filepath)
+				
+				folder = filepath.replace(".avi", "")
+				
+				if not os.path.isdir(folder):
+						os.mkdir(folder)
+						
+				while True:	
+						filename_initial = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
+						if os.path.isfile(filename_initial):
+							l = l + 1
+						else:
+							break
+				
 				while True:
-			#check frame is there, and read it in
 					ret, frame = video_capture.read()
-			#if at end of video, break out of loop
 					if (ret != True):
 						break
+					
+					filename_tif = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".tif"
+					filename_fits = folder+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
 						
-			
-			#construct the filenames
-					filename_tif = group+"/"+file+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".tif"
-					filename_fits = group+"/"+file+"/"+str(Imtype)+"_"+str(l)+"_"+str(n)+".fits"
-			
-			#write the TIF
 					cv2.imwrite(filename_tif,frame)
-			
-			#convert to FITS and remove TIF
+				
 					hdu = fits.PrimaryHDU()
 					im = Image.open(filename_tif)
 					hdu.data = np.array(im)
 					hdu.writeto(filename_fits, overwrite = True)
 					if switch != True: 
 						os.remove(filename_tif)
-			
-			#increment counter
 					n = n+1
 				l = l+1
 			else:

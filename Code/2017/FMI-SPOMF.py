b814f13d9fe39a928820b7ab9ad4f1f08f9c8c28
Chris <c.jduffy@me.com>
2017-03-14 16:56:59 +0000

    fdjkghsdlfghiusdhgidsfhgsldutghkdusgh. I want to die

--- Code/2017/FMI-SPOMF.py
+++ Code/2017/FMI-SPOMF.py
@@ -3,8 +3,33 @@ from scipy.fftpack import fft2, ifft2
 from scipy.ndimage import rotate
 import scipy.ndimage.interpolation as ndii 
 import math
+from scipy.signal import convolve2d
 #Final version won't need PIL
 from PIL import Image
+
+def _nd_window(data, filter_function):
+    """
+    Performs an in-place windowing on N-dimensional spatial-domain data.
+    This is done to mitigate boundary effects in the FFT.
+
+    Parameters
+    ----------
+    data : ndarray
+           Input data to be windowed, modified in place.
+    filter_function : 1D window generation function
+           Function should accept one argument: the window length.
+           Example: scipy.signal.hamming
+    """
+    for axis, axis_size in enumerate(data.shape):
+        # set up shape for numpy broadcasting
+        filter_shape = [1, ] * data.ndim
+        filter_shape[axis] = axis_size
+        window = filter_function(axis_size).reshape(filter_shape)
+        # scale the window intensities to maintain image intensity
+        window = np.power(window, (1.0/data.ndim))
+        data = data * np.float64(window)
+        
+    return data
 
 def logpolar(image, i_0, j_0, p_n=None, t_n=None):
     (i_n, j_n) = image.shape[:2]
@@ -53,16 +78,23 @@ def FMI(data1, data2):
 	centre_x_2 = max_x_2/2
 	centre_y_2 = max_y_2/2
 	
-	LP1 = logpolar(data1, centre_x_1, centre_y_1)
-	LP2 = logpolar(data2, centre_x_2, centre_y_2)
+	try:
+		data1_W = _nd_window(data1, np.hanning)
+		data2_W = _nd_window(data2, np.hanning)
+	except:
+		data1_W = _nd_window(data1[:,:,0], np.hanning)
+		data2_W = _nd_window(data2[:,:,0], np.hanning)
+	
+	LP1 = logpolar(data1_W, centre_x_1, centre_y_1)
+	LP2 = logpolar(data2_W, centre_x_2, centre_y_2)
 	
 	##Fourier Time!
 	
 	transform_image_1 = fft2(LP1)
 	transform_image_2 = fft2(LP2)
 		
-	phase_image_1 = np.imag(transform_image_1)
-	phase_image_2 = np.imag(transform_image_2)
+	phase_image_1 = transform_image_1
+	phase_image_2 = transform_image_2
 		
 	if phase_image_1.shape != phase_image_2.shape:
 		try:
@@ -71,11 +103,11 @@ def FMI(data1, data2):
 			try:
 				x = x2 - x1
 				y = y2 - y1
-				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0)), 'edge')
+				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0) ), 'edge')
 			except:
 				x = x1 - x2
 				y = y1 - y2
-				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0)), 'edge')
+				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0) ), 'edge')
 			
 		except:
 			x1, y1, colour = phase_image_1.shape
@@ -88,9 +120,12 @@ def FMI(data1, data2):
 				x = x1 - x2
 				y = y1 - y2
 				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0), (0,0) ), 'edge')
+	#SPOMF = np.multiply(phase_image_2, phase_image_1)
+	#iSPMF = ifft2(SPOMF)
 	
-	SPOMF = np.exp(np.subtract(phase_image_2, phase_image_1))
-	iSPMF = ifft2(SPOMF)
+	r0 = np.abs(phase_image_1) * np.abs(phase_image_2)
+	iSPMF = np.abs(ifft2((phase_image_1.conjugate() * phase_image_2) / r0))
+	
 	
 	return iSPMF
 
@@ -98,10 +133,11 @@ def FMI(data1, data2):
 
 ##Currently Hard wired for something in my working directory as I have decided how best to loop this yet.
 image1 = Image.open('scifestt.jpg')
-image2 = Image.open('scifestt.jpg')
+image2 = Image.open('scifestt1.jpg')
 
 data1 = np.array((image1))
 data2 = np.array((image2))
+#data2 = rotate(data2, 10, reshape = True, mode = 'nearest')
 
 iSPMF1 = FMI(data1, data2)
 
@@ -110,14 +146,20 @@ try:
 except:
 	rotation, scale, colour = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
 scaling_factor = np.exp(scale)
+angle = 180.0 * rotation / iSPMF1.shape[0]
+print(angle)
 print(rotation)
-print(scaling_factor)
+print(scale)
 
 try:
 	n, m = (data2.shape/scaling_factor)
 	n = int(n)
 	m = int(m)
-	scaled_image = data2.resize((n,m))
+	scaled_image = np.resize(data2, (n,m))
+
+	scaled_image_2 = scaled_image
+	rot1 = rotate(scaled_image, rotation, reshape = True)
+	rot2 = rotate(scaled_image_2, rotation+180, reshape = True)
 	
 except:
 	n, m, colour = (data2.shape/scaling_factor)
@@ -126,10 +168,9 @@ except:
 	colour = int(colour)
 	scaled_image = np.resize(data2, (n,m,colour))
 	
-
-scaled_image_2 = scaled_image
-rot1 = rotate(scaled_image, rotation, axes=(0,1), reshape = False)
-rot2 = rotate(scaled_image_2, rotation+180, reshape = False)
+	scaled_image_2 = scaled_image
+	rot1 = rotate(scaled_image, rotation, axes=(0,1), reshape = False)
+	rot2 = rotate(scaled_image_2, rotation+180, axes=(0,1), reshape = False)
 
 iSPMF2 = FMI(data1, rot1)
 iSPMF3 = FMI(data1, rot2)

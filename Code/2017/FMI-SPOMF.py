748bd0fc725f954ad1253817f6ec2692e55e5350
Chris <c.jduffy@me.com>
2017-03-16 03:30:55 +0000

    There were changes made. And the changes were good. The changes however dislike the colour world. They long for a simpler time where there was none of this new fangled colour nonsense. Back when everything was simple. When there was less Fourier Transforms. Those were the days>

--- Code/2017/FMI-SPOMF.py
+++ Code/2017/FMI-SPOMF.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.fftpack import fft2, ifft2
+from scipy.fftpack import fft2, ifft2, fftshift
 from scipy.ndimage import rotate
 import scipy.ndimage.interpolation as ndii 
 import math
@@ -30,101 +30,83 @@ def _nd_window(data, filter_function):
         data = data * np.float64(window)
         
     return data
-
-def logpolar(image, i_0, j_0, p_n=None, t_n=None):
-    (i_n, j_n) = image.shape[:2]
     
-    i_c = max(i_0, i_n - i_0)
-    j_c = max(j_0, j_n - j_0)
-    d_c = (i_c ** 2 + j_c ** 2) ** 0.5
-
-    if p_n == None:
-        p_n = int(math.ceil(d_c))
+def Logpolar(image, angles=None, radii=None):
+    """Return log-polar transformed image and log base."""
+    shape = image.shape
+    center = shape[0] / 2, shape[1] / 2
+    if angles is None:
+        angles = shape[0]
+    if radii is None:
+        radii = shape[1]
+    theta = np.empty((angles, radii), dtype=np.float64)
+    theta.T[:] = -np.linspace(0, np.pi, angles, endpoint=False)
+    #d = radii
+    d = np.hypot(shape[0]-center[0], shape[1]-center[1])
+    log_base = 10.0 ** (math.log10(d) / (radii))
+    radius = np.empty_like(theta)
+    radius[:] = np.power(log_base, np.arange(radii, dtype=np.float64)) - 1.0
+    x = radius * np.sin(theta) + center[0]
+    y = radius * np.cos(theta) + center[1]
+    output = np.empty_like(x)
+    output = ndii.map_coordinates(image, [x, y])
+    return output, log_base
     
-    if t_n == None:
-        t_n = j_n
-    
-    p_s = math.log(d_c) / p_n
-    t_s = 2.0 * np.pi / t_n
-    
-    transformed = np.zeros((p_n, t_n) + image.shape[2:], dtype=image.dtype)
-
-    for p in range(0, p_n):
-        p_exp = math.exp(p * p_s)
-        for t in range(0, t_n):
-            t_rad = t * t_s
-
-            i = int(i_0 + p_exp * math.sin(t_rad))
-            j = int(j_0 + p_exp * math.cos(t_rad))
-
-            if 0 <= i < i_n and 0 <= j < j_n:
-                transformed[p, t] = image[i, j]
-
-    return transformed
+def makeSquare(image):
+	if image.shape[0] <= image.shape[1]:
+		diff_x = image.shape[1] - image.shape[0]
+	else:
+		diff_x = 0
+	if image.shape[1] <= image.shape[0]:
+		diff_y = image.shape[0] - image.shape[1]
+	else:
+		diff_y = 0	
+	square = np.lib.pad(image, ((diff_x,0), (diff_y,0)), 'edge')
+	return square
 	
-
-def FMI(data1, data2):
+def shaping(data1, data2):
+	x1, y1 = data1.shape
+	x2, y2 = data2.shape
 	try:
-		max_x_1, max_y_1 = data1.shape
-		max_x_2, max_y_2 = data2.shape
+		x = x2 - x1
+		y = y2 - y1
+		data1 = np.lib.pad(data1, ((x,0), (y,0) ), 'edge')
 	except:
-		max_x_1, max_y_1, colour = data1.shape
-		max_x_2, max_y_2, colour = data2.shape
-		
-	##Calcylate the centres for both images
-	centre_x_1 = max_x_1/2
-	centre_y_1 = max_y_1/2
+		x = x1 - x2
+		y = y1 - y2
+		data2 = np.lib.pad(data2, ((x,0), (y,0) ), 'edge')
+	return data1, data2
+
+def FMI(data1, data2, polar):
+	if data1.shape[0] != data1.shape[1]:
+		data1 = makeSquare(data1)	
+	if data2.shape[0] != data2.shape[1]:
+		data2 = makeSquare(data2)
 	
-	centre_x_2 = max_x_2/2
-	centre_y_2 = max_y_2/2
+	if data1.shape != data2.shape:
+		data1, data2 = shaping(data1, data2)
+	if polar is False:
+		data1 = fftshift(abs(fft2(data1)))
+		data2 = fftshift(abs(fft2(data2)))
 	
-	try:
 		data1_W = _nd_window(data1, np.hanning)
 		data2_W = _nd_window(data2, np.hanning)
-	except:
-		data1_W = _nd_window(data1[:,:,0], np.hanning)
-		data2_W = _nd_window(data2[:,:,0], np.hanning)
+
 	
-	LP1 = logpolar(data1_W, centre_x_1, centre_y_1)
-	LP2 = logpolar(data2_W, centre_x_2, centre_y_2)
-	
-	##Fourier Time!
-	
-	transform_image_1 = fft2(LP1)
-	transform_image_2 = fft2(LP2)
+		LP1, logbase = Logpolar(data1_W)
+		LP2, logbase = Logpolar(data2_W)
+	else:
+		LP1 = data1
+		LP2 = data2
 		
-	phase_image_1 = transform_image_1
-	phase_image_2 = transform_image_2
-		
-	if phase_image_1.shape != phase_image_2.shape:
-		try:
-			x1, y1 = phase_image_1.shape
-			x2, y2 = phase_image_2.shape
-			try:
-				x = x2 - x1
-				y = y2 - y1
-				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0) ), 'edge')
-			except:
-				x = x1 - x2
-				y = y1 - y2
-				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0) ), 'edge')
-			
-		except:
-			x1, y1, colour = phase_image_1.shape
-			x2, y2, colour = phase_image_2.shape
-			try:
-				x = x2 - x1
-				y = y2 - y1
-				phase_image_1 = np.lib.pad(phase_image_1, ((x,0,), (y,0), (0,0) ), 'edge')
-			except:
-				x = x1 - x2
-				y = y1 - y2
-				phase_image_2 = np.lib.pad(phase_image_2, ((x,0,), (y,0), (0,0) ), 'edge')
+	phase_image_1 = fft2(LP1)
+	phase_image_2 = fft2(LP2)
+
 	#SPOMF = np.multiply(phase_image_2, phase_image_1)
 	#iSPMF = ifft2(SPOMF)
 	
-	r0 = np.abs(phase_image_1) * np.abs(phase_image_2)
-	iSPMF = np.abs(ifft2((phase_image_1.conjugate() * phase_image_2) / r0))
+	r0 = abs(phase_image_1) * abs(phase_image_2)
+	iSPMF = abs(ifft2((phase_image_1.conjugate() * phase_image_2) / r0))
 	
 	
 	return iSPMF
@@ -132,48 +114,56 @@ def FMI(data1, data2):
 ##Image read in
 
 ##Currently Hard wired for something in my working directory as I have decided how best to loop this yet.
-image1 = Image.open('scifestt.jpg')
-image2 = Image.open('scifestt1.jpg')
+image1 = Image.open('outfile.jpg')
+image2 = Image.open('outfile.jpg')
 
 data1 = np.array((image1))
 data2 = np.array((image2))
-#data2 = rotate(data2, 10, reshape = True, mode = 'nearest')
 
-iSPMF1 = FMI(data1, data2)
+if data1.ndim == 3:
+	data1 = data1[:,:,0]
+if data2.ndim == 3:
+	data2 = data2[:,:,0]
+data2 = rotate(data2, 180, reshape = False)
 
-try:
-	rotation, scale = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
-except:
-	rotation, scale, colour = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
+iSPMF1 = FMI(data1, data2, False)
+
+rotation, scale = np.unravel_index(np.argmax(iSPMF1), iSPMF1.shape)
 scaling_factor = np.exp(scale)
 angle = 180.0 * rotation / iSPMF1.shape[0]
+
+
+if scaling_factor > 1.8:
+	angle = -1 * angle
+	scaling_factor = 1.0 / scaling_factor
+	print(scaling_factor)
+	if scale > 1.8:
+		raise ValueError("You broke it, the scale differnce was too large") #GTK waning probably better here also
 print(angle)
-print(rotation)
-print(scale)
+print(scaling_factor)
 
-try:
-	n, m = (data2.shape/scaling_factor)
-	n = int(n)
-	m = int(m)
-	scaled_image = np.resize(data2, (n,m))
+n, m = (data2.shape/scaling_factor)
+n = int(n)
+m = int(m)
+scaled_image = np.resize(data2, (n,m))
 
-	scaled_image_2 = scaled_image
-	rot1 = rotate(scaled_image, rotation, reshape = True)
-	rot2 = rotate(scaled_image_2, rotation+180, reshape = True)
+scaled_image_2 = scaled_image
+rot1 = rotate(scaled_image, angle, reshape = False)
+rot2 = rotate(scaled_image_2, angle+180, reshape = False)
 	
-except:
-	n, m, colour = (data2.shape/scaling_factor)
-	n = int(n)
-	m = int(m)
-	colour = int(colour)
-	scaled_image = np.resize(data2, (n,m,colour))
-	
-	scaled_image_2 = scaled_image
-	rot1 = rotate(scaled_image, rotation, axes=(0,1), reshape = False)
-	rot2 = rotate(scaled_image_2, rotation+180, axes=(0,1), reshape = False)
 
-iSPMF2 = FMI(data1, rot1)
-iSPMF3 = FMI(data1, rot2)
+iSPMF2 = FMI(data1, rot1, True)
+iSPMF3 = FMI(data1, rot2, True)
 
 A = np.amax(iSPMF2)
 B = np.amax(iSPMF3)
+print(A)
+print(B)
+A=B
+if A > B:
+	print("Do your thing with iSPMF2")
+elif B > A:
+	print("Do your thing with iSPMF3")
+else:
+	print("Image appears identical under all roations after scaling, attempting to proceed but results may be imperfect")
+	#Probably worth calling either a GTK wanring window if this condition is met or a terminal wanring but I am too tired to work out the latter and the former is not my skill set.

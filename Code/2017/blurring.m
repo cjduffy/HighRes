020c44a598bb62ccb959642c31581496a57a3888
Chris <c.jduffy@me.com>
2017-04-06 11:54:26 +0100

    updated code to make more blurred images with smaller incriments in blur

--- Code/2017/blurring.m
+++ Code/2017/blurring.m
@@ -1,6 +1,6 @@
 image = imread('Lenna.png');
 n = 1;
-for x = 1:5:50
+for x = 1:2:50
     blurred = imgaussfilt(image, n);
     filename = sprintf('blurred_%d.png', n);
     imwrite(blurred, filename)
